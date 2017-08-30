5fdb5b8739c0433e302aeaff943bde387a24f884
Richard <Richard@DESKTOP-CDJU9C9>
2017-08-30 09:26:25 +0100

    Add project files.

---
+++ h2hBrainGames/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(h2hBrainGames.Startup))]
+namespace h2hBrainGames
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
